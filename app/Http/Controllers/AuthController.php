ea1fbc2a7b091134a2474962e21fbc04730652c1
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-08-10 15:07:18 -0300

    Jwt auth implemented

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+
+class AuthController extends Controller
+{
+
+	private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    
+    public function index() {
+    	return response()->json([
+    		'message' => 'Hi bro'
+    	]);
+    }
+
+    public function register(Request $request)
+    {
+        $user = $this->user->create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password'))
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User created successfully',
+            'data' => $user
+        ]);
+    }
+
+
+   public function signin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $access_token = null;
+
+        try {
+            if (! $access_token = JWTAuth::attempt($credentials))
+            {
+                return response()->json([
+                    'error' => 'invalid_credentials'
+                ], 401);
+            }
+        } catch (JWTException $ex) {
+            return response()->json([
+                'error' => 'could_not_create_token'
+            ], 500);
+        }
+
+        $token_type = 'bearer';
+        $expires_in = JWTAuth::getPayload($access_token)->get('exp');
+
+        return response()->json(
+            compact(['access_token', 'token_type', 'expires_in'])
+        );
+    }
+
+    public function getAuthUser(Request $request)
+    {
+        $user = JWTAuth::toUser($request->access_token);
+        return response()->json(['result' => $user]);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['jwt.auth'])->group(function() { 
+	
+	Route::post('/', 'AuthController@index');
+	
 });
+
+Route::post('/register', 'AuthController@register');
+
+Route::post('/login', 'AuthController@signin');
